c5e862ff1c36b2f0ff59ba60b4230d6805427f6d
Adam Rushford <arushford@protonmail.me>
2024-12-23 07:18:59 -0500

    init commit

---
+++ engine/src/engine.h
@@ -0,0 +1,6 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+void displayMessage();
+
+#endif // EXAMPLE_H
---
+++ engine/src/main.cpp
@@ -0,0 +1,6 @@
+#include "engine.h" 
+#include <iostream>
+
+void displayMessage() {
+    std::cout << "Hello from the static library!" << std::endl;
+}
